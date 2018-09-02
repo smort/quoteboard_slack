28724746b8f109aa9886c69ce442167def4795f2
Sam Morton <smort@users.noreply.github.com>
2018-09-01 22:12:31 -0600

    add challenge

--- cmd/handle-mention/main.go
+++ cmd/handle-mention/main.go
@@ -1,21 +1,52 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/nlopes/slack"
+	"github.com/nlopes/slack/slackevents"
 )
 
-func handleEvent(event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+var token = os.Getenv("SLACK_OATH")
+var api = slack.New(token)
+
+func handleEvent(event events.APIGatewayProxyRequest) events.APIGatewayProxyResponse {
 	fmt.Println(fmt.Sprintf("Call received: %+v", event))
 
-	// client := slack.New("test")
+	slackEvent, err := slackevents.ParseEvent(json.RawMessage(event.Body))
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: 500,
+			Headers:    map[string]string{"Content-Type": "application/json"},
+			Body:       "{message:\"Error parsing Slack event\"}",
+		}
+	}
+
+	if slackEvent.Type == slackevents.URLVerification {
+		var r *slackevents.ChallengeResponse
+		err := json.Unmarshal([]byte(event.Body), &r)
+		if err != nil {
+			return events.APIGatewayProxyResponse{
+				StatusCode: 500,
+				Headers:    map[string]string{"Content-Type": "text/plain"},
+				Body:       r.Challenge,
+			}
+		}
+
+		return events.APIGatewayProxyResponse{
+			StatusCode: 200,
+		}
+	}
 
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
+		Headers:    map[string]string{"Content-Type": "application/json"},
 		Body:       "{message:\"test\"}",
-	}, nil
+	}
 }
 
 func main() {
